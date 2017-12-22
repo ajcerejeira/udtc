763f85946d38d652c93ef07148a4391085385b3e
Octávio Maia <a71369@alunos.uminho.pt>
2017-12-22 17:04:30 +0000

    Updated the Travel dependancy and added the respective View Also updated the Controller in order to work with Travel

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,10 +1,8 @@
 package Controller;
 
 import Model.Chronometer;
-import View.NotebookView;
-import View.ChronoView;
-import View.Menu;
-import View.Option;
+import Model.Travels;
+import View.*;
 
 
 public class Controller implements Runnable{
@@ -13,7 +11,8 @@ public class Controller implements Runnable{
     public void run(){
         Menu menu = new Menu(new Option[] {
                 new Option("Chronometer", () -> new ChronoView(new Chronometer()).run()),
-                new Option("Notebook", () -> new NotebookView().run())
+                new Option("Notebook", () -> new NotebookView().run()),
+                new Option("Traveling", () -> new TravelView(new Travels()).run())
         });
 
         while (true)
--- src/Model/ITravels.java
+++ src/Model/ITravels.java
@@ -1,4 +1,19 @@
 package Model;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ITravels {
+    void addTravel(Travel t);
+    void removeTravel(int n);
+    List<Travel> getTravels();
+    List<Travel> cheapestTravels(int n);
+    List<Travel> mostExpensiveTravels(int n);
+    List<Travel> shortestTravels(int n);
+    List<Travel> longestTravels(int n);
+    List<Travel> travelsBetweenDates(LocalDateTime d1, LocalDateTime d2);
+    Duration timeUntilNextTravel();
+    Duration timeUntilLastTravel();
+    Travels clone();
 }
