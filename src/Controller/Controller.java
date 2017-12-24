ddc5953b94e3bdb0df5bc3dc6c2e005c5b540f1e
Octávio Maia <a71369@alunos.uminho.pt>
2017-12-24 14:25:24 +0000

    Updated Controller, Calendar, Chronometer and Static

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -7,14 +7,12 @@ import Utils.UI.Option;
 import View.*;
 
 public class Controller implements Runnable{
-    private Chronometer chronometer;
     private Notebook notebook;
     private DateCalculator dateCalculator;
     private Travels travels;
     private Calendar calendar;
 
     public Controller() {
-        this.chronometer = new Chronometer();
         this.notebook = new Notebook();
         this.dateCalculator = new DateCalculator();
         this.travels = new Travels();
@@ -23,16 +21,14 @@ public class Controller implements Runnable{
 
     @Override
     public void run(){
-        Chronometer chronometer = new Chronometer();
-
         while (true) {
             new UI(new Runnable[] {
                     new Title("UDTC", 1),
                     new Menu(new Option[] {
-                            new Option("C", "Chronometer", new ChronoView(this.chronometer)),
+                            new Option("C", "Chronometer", new ChronoView(new Chronometer())),
                             new Option("N", "Notebook", new NotebookView(this.notebook)),
-                            new Option("T","Traveling", new TravelView(this.travels)),
-                            new Option("D","DateTime Calculator", new DateView(this.dateCalculator)),
+                            new Option("T", "Traveling", new TravelView(this.travels)),
+                            new Option("D", "DateTime Calculator", new DateView(this.dateCalculator)),
                             new Option("CL","Calendar", new CalendarView(this.calendar)),
                             new Option("Q", "Quit", () -> System.exit(-1)),
                     }),
